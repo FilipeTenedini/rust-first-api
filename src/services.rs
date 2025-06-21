fb3b46f76a5be426ff99ece359cc2118e3ef8b7e
FilipeTenedini <tenedinifilipe@gmail.com>
2025-06-21 19:59:03 -0300

    feat: create_task_handler

--- src/services.rs
+++ src/services.rs
@@ -1,6 +1,7 @@
+use crate::{AppState, model::TaskModel, schema::CreateTaskSchema};
 use actix_web::{
-    HttpResponse, Responder, get,
-    web::{ServiceConfig, scope},
+    HttpResponse, Responder, get, post,
+    web::{Data, Json, ServiceConfig, scope},
 };
 use serde_json::json;
 
@@ -13,7 +14,37 @@ async fn health_checker_handler() -> impl Responder {
         "message": MESSAGE
     }));
 }
+
+#[post("/task")]
+async fn create_task_handler(body: Json<CreateTaskSchema>, data: Data<AppState>) -> impl Responder {
+    match sqlx::query_as!(
+        TaskModel,
+        "INSERT INTO tasks (title, content) VALUES ($1, $2) RETURNING *",
+        body.title,
+        body.content
+    )
+    .fetch_one(&data.db)
+    .await
+    {
+        Ok(task) => {
+            let task_response = json!({
+                "status": "success",
+                "data": task
+            });
+            return HttpResponse::Ok().json(task_response);
+        }
+        Err(e) => {
+            return HttpResponse::InternalServerError().json(json!({
+                "status": "error",
+                "message": format!("{:?}", e)
+            }));
+        }
+    }
+}
+
 pub fn config(cfg: &mut ServiceConfig) {
-    let scope = scope("/api").service(health_checker_handler);
+    let scope = scope("/api")
+        .service(health_checker_handler)
+        .service(create_task_handler);
     cfg.service(scope);
 }
