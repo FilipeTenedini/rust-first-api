f399c3a153af05a35d45b82237a9fbf4482de17f
FilipeTenedini <tenedinifilipe@gmail.com>
2025-06-22 19:48:10 -0300

    feat: get by id and delete task

--- src/services.rs
+++ src/services.rs
@@ -4,10 +4,11 @@ use crate::{
     schema::{CreateTaskSchema, FilterOptions},
 };
 use actix_web::{
-    HttpResponse, Responder, get, post,
-    web::{Data, Json, Query, ServiceConfig, scope},
+    HttpResponse, Responder, delete, get, post,
+    web::{Data, Json, Path, Query, ServiceConfig, scope},
 };
 use serde_json::json;
+use uuid::Uuid;
 
 #[get("/health-checker")]
 async fn health_checker_handler() -> impl Responder {
@@ -46,6 +47,29 @@ async fn create_task_handler(body: Json<CreateTaskSchema>, data: Data<AppState>)
     }
 }
 
+#[get("task/{id}")]
+async fn get_task_by_id_handler(path: Path<Uuid>, data: Data<AppState>) -> impl Responder {
+    let task = sqlx::query_as!(
+        TaskModel,
+        "SELECT * FROM tasks WHERE id = $1",
+        path.into_inner(),
+    )
+    .fetch_one(&data.db)
+    .await;
+
+    match task {
+        Ok(task) => {
+            return HttpResponse::Ok().json(json!({"status": "success", "data": task}));
+        }
+        Err(e) => {
+            return HttpResponse::NotFound().json(json!({
+                "status": "error",
+                "message": format!("{:?}", e)
+            }));
+        }
+    }
+}
+
 #[get("/task")]
 async fn get_all_tasks_handler(
     filters: Query<FilterOptions>,
@@ -75,11 +99,36 @@ async fn get_all_tasks_handler(
     }
 }
 
+#[delete("task/{id}")]
+async fn delete_task_handler(path: Path<Uuid>, data: Data<AppState>) -> impl Responder {
+    let result = sqlx::query_as!(
+        TaskModel,
+        "DELETE FROM tasks WHERE id = $1",
+        path.into_inner(),
+    )
+    .execute(&data.db)
+    .await;
+
+    match result {
+        Ok(_) => {
+            return HttpResponse::Ok().json(json!({"status": "success"}));
+        }
+        Err(e) => {
+            return HttpResponse::NotFound().json(json!({
+                "status": "error",
+                "message": format!("{:?}", e)
+            }));
+        }
+    }
+}
+
 pub fn config(cfg: &mut ServiceConfig) {
     let scope = scope("/api")
         .service(health_checker_handler)
         .service(create_task_handler)
-        .service(get_all_tasks_handler);
+        .service(get_all_tasks_handler)
+        .service(get_task_by_id_handler)
+        .service(delete_task_handler);
 
     cfg.service(scope);
 }
